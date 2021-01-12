b3080c93755fbba88b1c45e441fa16c47ce8f546
Charlie Knight <williamchknight@gmail.com>
2021-01-12 14:36:06 -0800

    commit tree f928a2758289dc52abf85a2ec6a613e29bb04e97

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'not me'
+
+function upperCaseCustomerName(){ 
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'you'
+}
+
